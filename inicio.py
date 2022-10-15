dcfb69a39e61286d8c5ec68e3e8f562697f5658b
MicaelaEdith <romeromicaelaedith@gmail.com>
2022-10-15 12:44:35 -0300

    inicio

---
+++ inicio.py
@@ -0,0 +1,7 @@
+import pygame
+
+class Inicio():
+    def __init__(self, pantalla):
+        self.pantalla=pantalla
+
+        ##################--Definir: seleccion de acciones para ingresar, modificar, listar y eliminar jugadores.
